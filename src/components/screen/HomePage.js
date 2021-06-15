357fee2dc79da66b5824a45701fc8248bb78fb6e
allan duminy <allanduminy@protonmail.com>
2021-06-15 10:23:44 +0200

    création de la screen homepage et du formulaire pour ajouter un livre

--- src/components/screen/HomePage.js
+++ src/components/screen/HomePage.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { TextInput } from 'react-native-paper';
+import { View, Button } from 'react-native'
+import { TextInput } from 'react-native-paper'
+
+import { BookSchema } from '../../schemas/BookSchema'
+import Realm from "realm"
+
 
 export default function HomePage() {
-    const [text, setText] = React.useState('');
-  return (
-    <TextInput
-      label="Email"
-      value={text}
-      onChangeText={text => setText(text)}
-    />
-  );
+    const [titre, setTitre] = React.useState('')
+    const [author, setAuthor] = React.useState('');
+    const [category, setCategory] = React.useState('');
+
+    const addBook = () => {realm.write(() => {
+    // Assign a newly-created instance to the variable.
+        BookSchema = realm.create("Book", { author: author, category: category, title: titre });
+    })};
+    
+    return (
+        <View>
+            <TextInput
+                label="Titre du livre"
+                value={titre}
+                onChangeText={titre => setTitre(titre)}
+            />
+
+            <TextInput
+                label="Auteur"
+                value={author}
+                onChangeText={author => setAuthor(author)}
+            />
+
+            <TextInput
+                label="Catégorie"
+                value={category}
+                onChangeText={category => setCategory(category)}
+            />
+
+            <Button title="Envoyer" onPress={() => addBook()}/>
+        </View>
+    );
 }
